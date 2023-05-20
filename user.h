198f8f166538803853c08b1f845d6c5c076a15b1
baejae <37645490+baejaeho18@users.noreply.github.com>
2023-05-20 23:51:27 +0900

    Create user.h

---
+++ user.h
@@ -0,0 +1,6 @@
+typedef struct{ } User;
+int insertUser(User *u ){}
+void listUser(User *u[], int count){}
+int updateUser(User *u ){}
+int selectMenu(){}
+int selectDataNo(User *user[],int count){}
